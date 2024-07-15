b78becfd1ad93b709210ae0c83b9b20aa1e9379a
arsenijhavanov <claudinm@student.21-school.ru>
2024-07-15 14:19:43 +0500

    try to fix 3

--- internal/storage/memory.go
+++ internal/storage/memory.go
@@ -9,55 +9,31 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// AllMetrics хранит все метрики для сериализации
-type AllMetrics struct {
-	Gauge   map[string]float64 `json:"gauges"`
-	Counter map[string]int64   `json:"counters"`
-}
-
-// saveStorageToFile сохраняет текущие метрики в файл
 func saveStorageToFile(s *MemStorage, filePath string) error {
-	metrics := AllMetrics{
-		Counter: s.counters,
-		Gauge:   s.gauges,
-	}
-	// Проверка и создание файла, если его нет
-	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+	var metrics AllMetrics
+	metrics.Counter = s.counters
+	metrics.Gauge = s.gauges
 
 	data, err := json.MarshalIndent(metrics, "", "   ")
 	if err != nil {
 		return err
 	}
 
-	_, err = file.Write(data)
-	return err
+	return os.WriteFile(filePath, data, 0666)
+
 }
 
-// Dump периодически сохраняет метрики из хранилища в файл.
 func Dump(s *MemStorage, filePath string, storeInterval int) error {
 	dir, _ := path.Split(filePath)
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		err := os.MkdirAll(dir, 0755)
+		err := os.MkdirAll(dir, 0666)
 		if err != nil {
 			logrus.Info(err)
 		}
 	}
-
-	// Проверка и создание файла, если его нет
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		_, err := os.Create(filePath)
-		if err != nil {
-			logrus.Info(err)
-		}
-	}
-
-	ticker := time.NewTicker(time.Duration(storeInterval) * time.Second)
-	defer ticker.Stop()
-	for range ticker.C {
+	pollTicker := time.NewTicker(time.Duration(storeInterval) * time.Second)
+	defer pollTicker.Stop()
+	for range pollTicker.C {
 		err := saveStorageToFile(s, filePath)
 		if err != nil {
 			logrus.Info(err)
@@ -66,26 +42,22 @@ func Dump(s *MemStorage, filePath string, storeInterval int) error {
 	return nil
 }
 
-// LoadStorageFromFile загружает метрики из файла в хранилище
 func LoadStorageFromFile(s *MemStorage, filePath string) error {
 	file, err := os.ReadFile(filePath)
 	if err != nil {
 		logrus.Info(err)
-		return err
 	}
 
 	var data AllMetrics
 	if err := json.Unmarshal(file, &data); err != nil {
 		logrus.Info(err)
-		return err
 	}
 
-	for name, value := range data.Counter {
-		s.UpdateCounter(name, value)
+	if len(data.Counter) != 0 {
+		s.UpdateCounterData(data.Counter)
 	}
-	for name, value := range data.Gauge {
-		s.UpdateGauge(name, value)
+	if len(data.Gauge) != 0 {
+		s.UpdateGaugeData(data.Gauge)
 	}
-
-	return nil
+	return err
 }
